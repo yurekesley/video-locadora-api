dbcc5ceff57798134ff4429a385054a6dffa3b6c
Yure Kesley <yurekesley@gmail.com>
2019-09-16 00:03:26 -0300

    versionando a api

--- Gemfile
+++ Gemfile
@@ -5,10 +5,17 @@ ruby '2.6.4'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 6.0.0'
+
 # Use postgresql as the database for Active Record
 gem 'pg', '>= 0.18', '< 2.0'
+
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
+
+# Rack Middleware for handling Cross-Origin Resource Sharing (CORS), which makes cross-origin AJAX possible.
+gem 'rack-cors'
+
+
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 # gem 'jbuilder', '~> 2.7'
 # Use Redis adapter to run Action Cable in production
--- config/application.rb
+++ config/application.rb
@@ -21,8 +21,17 @@ Bundler.require(*Rails.groups)
 
 module VideoLocadora
   class Application < Rails::Application
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+
+    # CORS Middleware
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :delete, :put, :patch, :options, :head]
+      end
+    end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
@@ -34,4 +43,6 @@ module VideoLocadora
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
   end
+
+
 end
